4480e9dc6d814a9522579f3dc3d97c9e4d62c056
ofekats <ofekats@gmail.com>
2023-03-16 11:23:46 +0200

    1

---
+++ AdptArray.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+PAdptArray CreateAdptArray(COPY_FUNC, DEL_FUNC,PRINT_FUNC);
+void DeleteAdptArray(PAdptArray);
+Result SetAdptArrayAt(PAdptArray, int, PElement);
+PElement GetAdptArrayAt(PAdptArray, int);
+int GetAdptArraySize(PAdptArray);
+void PrintDB(PAdptArray);
